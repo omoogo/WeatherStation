b38aeb11989c171ed20e48b7cfdccce530a7e502
Michael Broniola <mbroniola@gmail.com>
2018-05-03 21:02:48 -0700

    first commit

---
+++ WeatherStation/WeatherData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherStation
+{
+    public class WeatherData : ISubject
+    {
+        private List<IObserver> _observers;
+        private float _temperature;
+        private float _humidity;
+        private float _pressure;
+
+        public WeatherData()
+        {
+            _observers = new List<IObserver>();
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in _observers)
+            {
+                observer.Update(_temperature, _humidity, _pressure);
+            }
+        }
+
+        public void RegisterObserver(IObserver o)
+        {
+            _observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            _observers.Remove(o);
+        }
+
+        public void MeasurementsChanged()
+        {
+            NotifyObservers();
+        }
+
+        public void SetMeasurements(float temperature, float humidity, float pressure)
+        {
+            _temperature = temperature;
+            _humidity = humidity;
+            _pressure = pressure;
+            MeasurementsChanged();
+        }
+    }
+}
